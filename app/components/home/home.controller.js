7dc3cbf86ba951f7f7d0d510d47d8d275887d734
m.obpacher <m.obpacher@anfe.ma>
2016-05-28 10:32:19 +0200

    included angular-upload

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -20,8 +20,6 @@
             vm.HeroText = asyncService.retrievedData.HeroText;
 
             // subsections
-            vm.col0heading = "Subsections";
-            vm.col0text = "I may populate this with a microservice! Or have this be a separate view. This template uses Angular UI which is better than using the Angular router in my opinion.";
 
             return vm;
        }
