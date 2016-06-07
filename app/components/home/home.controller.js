97c52a879607ed98ac80a282f6638bdcf44dfc60
m.obpacher <m.obpacher@anfe.ma>
2016-06-07 10:14:27 +0200

    refactored confusing routes for homepage (heroText replaced)

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -13,16 +13,6 @@
             //services
             vm.angularstrapService = asyncService;
 
-            asyncService.getHeroText();
-
-            // from async service
-            vm.HeroHeader = asyncService.retrievedData.HeroHeader;
-            vm.HeroText = asyncService.retrievedData.HeroText;
-
-            // subsections
-            vm.col0heading = "Subsections";
-            vm.col0text = "I may populate this with a microservice! Or have this be a separate view. This template uses Angular UI which is better than using the Angular router in my opinion.";
-
             return vm;
        }
 })();
