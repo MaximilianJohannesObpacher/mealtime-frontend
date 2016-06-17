9b128d2e14983ca8a6897e1f78542a71fdf990f9
m.obpacher <m.obpacher@anfe.ma>
2016-06-17 15:06:52 +0200

    basic styling of meal detail page

--- app/components/meal-detail/meal-detail-controller.js
+++ app/components/meal-detail/meal-detail-controller.js
@@ -16,4 +16,16 @@ mealtime.controller("MealDetailController", function ($http, $scope, $stateParam
         });
     }
 
+    function initialize() {
+        var mapProp = {
+            center:new google.maps.LatLng(48.2625,11.6670),
+            zoom:16,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
 });
