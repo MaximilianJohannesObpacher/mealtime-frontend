8df151acdf137a25efb64f8dc6a9eb59bccfe8d5
Christoh Putz <chputz@gmx.de>
2016-07-01 15:32:38 +0200

    show profile

--- app/components/show-profile/show-profile-controller.js
+++ app/components/show-profile/show-profile-controller.js
@@ -3,13 +3,20 @@
  */
 angular.module('mealtime-frontend')
 
-    .controller('showProfileController', ['$scope','$http','globalService', function($scope, $http, globalService) {
-        var userData = globalService.loadGlobal();
-        var app = this;
-        app.prename = userData.prename;
-        app.lastname = userData.lastname;
-        app.email = userData.email;
-        app.address = userData.address;
-        app.description = userData.description;
-        app._id = userData._id;
+    .controller('showProfileController', ['$scope','$http','globalService', '$window', function($scope, $http, globalService, $window) {
+            console.log("inside show profile controller");
+            var userData = globalService.loadGlobal();
+            var app = this;
+            app.prename = userData.prename;
+            app.lastname = userData.lastname;
+            app.email = userData.email;
+            app.address = userData.address;
+            app.description = userData.description;
+            app._id = userData._id;
+
+            this.show = function () {
+                    console.log("in show ctrl call fun show");
+                    //globalService.storeGlobal(userData);
+                    $window.location.href = "#/profile/edit/"+app._id;
+            };
     }]);
