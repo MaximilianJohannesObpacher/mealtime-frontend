6103d5369f7e1d99342a350568ce88bb99d53303
m.obpacher <m.obpacher@anfe.ma>
2016-07-02 12:18:12 +0200

    Active listings overview page created!
    Functionality to delete listings added

--- app/components/profile-meals/profile-meals-controller.js
+++ app/components/profile-meals/profile-meals-controller.js
@@ -4,7 +4,7 @@
 
 angular.module('mealtime-frontend')
 
-    .controller('profileMealsController', ['$scope', '$http', '$stateParams', '$window', 'userService', function ($scope, $http, $stateParams, $window, userService) {
+    .controller('profileMealsController', ['$scope', '$http', '$stateParams', '$window', 'userService', 'ngToast', function ($scope, $http, $stateParams, $window, userService, ngToast) {
 
         console.log("Mealcontroller1!");
 
@@ -12,29 +12,59 @@ angular.module('mealtime-frontend')
             
             app = this;
 
+            loadMeals();
+
             console.log("Mealcontroller2!");
 
-            $scope.user_Id = userService.loadGlobal().user_Id;
             $scope.isLoggedIn = userService.loadGlobal() == null;
 
-            $http.post("http://localhost:3000/api/meals/" + $scope.user_Id,
-                {
-                    chefId: $scope.user_Id
-                })
-            //on success save user information globally
-                .then(function (response) {
-                        console.log("offerings: ", response.data);
-                        app.offerings = response.data;
+            function loadMeals() {
+                $http.post("http://localhost:3000/api/meals/" + userService.loadGlobal()._id,
+                    {
+                        chefId: userService.loadGlobal()._id
+                    })
+                //on success save user information globally
+                    .then(function (response) {
+                            console.log("offerings: ", response.data);
+                            app.offerings = response.data;
 
-                    },
+                        },
 
-                    function (response) {
-                        console.log("error on response!")
-                    }
-                )
+                        function (response) {
+                            console.log("error on response!")
+                        }
+                    )
+            }
 
+            this.deleteOffering = function (offeringId) {
+                console.log("indeleteOffering");
+
+                if(offeringId != null){
+                    $http.delete("http://localhost:3000/api/meals/" + offeringId,
+                        {
+                            meal_Id: offeringId
+                        })
+                        .then(function (response) {
+                                ngToast.create("Offering deleted successful!");
+                                loadMeals();
+                                $window.location.href = "/#/profile/meals/" + userService.loadGlobal()._id;
+                            },
+
+                            function (response) {
+                                console.log("error on response!")
+                            }
+                        );
+
+
+
+                }
+                else{
+                    ngToast.create("Offering doesn't exist anymore!");
+                }
+
+            };
         }
-
+            
         else
         {
             console.log("Mealcontroller3!");
